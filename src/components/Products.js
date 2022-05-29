39a42a63e1bad9bb6c7d83a12c394b78a72f6b33
AJ <ajabad.dev@gmail.com>
2022-05-29 12:07:53 +0800

    Added constants and state properties

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,6 +3,20 @@ import { Fragment } from "react";
 import axios from "axios";
 import Ad from "./Ad";
 const Products = (props) => {
+  const PAGE_SIZE = 20;
+  const PRODUCTS_BEFORE_AD = 20;
+  const { lastAd, setLastAd } = props;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isEndReached, setIsEndReached] = useState(false);
+  const sortBy = useRef("");
+  const isLoadingRef = useRef(true);
+  const cachedProducts = useRef(null);
+  const currentPage = useRef(1);
+  return (
+    <main>
+    </main>
+  );
 };
 
 export default Products;

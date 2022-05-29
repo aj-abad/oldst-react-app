bff56b231a3dc8d57157620bb100a83fd2c03d28
AJ <ajabad.dev@gmail.com>
2022-05-29 11:58:41 +0800

    Added container class

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ const App = () => {
   const [lastAd, setLastAd] = useState(null);
 
   return (
-    <div className='App'>
-      <header>
+    <div className="App">
+      <div className="container py-4 bg-white">
+        <header>
           <h1>Products Grid</h1>
         </header>
         <hr />
+      </div>
     </div>
   );
 }

764e42c964e39923b7de9adb2f1a72d4aaec9e70
AJ <ajabad.dev@gmail.com>
2022-05-29 11:59:25 +0800

    Added ad component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
       <div className="container py-4 bg-white">
         <header>
           <h1>Products Grid</h1>
+          <Ad lastAd={lastAd} setLastAd={setLastAd} />
         </header>
         <hr />
       </div>

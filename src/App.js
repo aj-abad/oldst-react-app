fbcd03e83dd481b47a871514e709719c0f600caa
AJ <ajabad.dev@gmail.com>
2022-05-29 12:06:06 +0800

    Added comment

--- src/App.js
+++ src/App.js
@@ -11,6 +11,11 @@ const App = () => {
           <Ad lastAd={lastAd} setLastAd={setLastAd} />
         </header>
         <hr />
+        {/* Passing the ad props to the products component 
+            because it needs to know which ad was displayed in
+            the header 
+            useContext might be better for more complex use cases
+          */}
       </div>
     </div>
   );

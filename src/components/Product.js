8bc7e1305ac1c09365a635b221e909a4921ad392
AJ <ajabad.dev@gmail.com>
2022-05-29 13:00:24 +0800

    Updated layout

--- src/components/Product.js
+++ src/components/Product.js
@@ -11,19 +11,22 @@ const Product = ({ product }) => {
   const formattedDate = getDateDiff(new Date(date));
 
   return (
-    <div className="product p-4">
-      <div className="row">
-        <div className="col-lg-4">
-          <span className="h4 d-block mb-2">Face <small className="text-muted">#{id}</small></span>
+    <div className="product mb-4">
+      <div className="product-info p-4">
+        <div className="mb-2">
+          <span className="h4 d-block mb-2">
+            Face <small className="text-muted">#{id}</small>
+          </span>
           <span className="h5">{formattedPrice}</span>
-          <br />
-          <small className="text-muted">Added {formattedDate}</small>
         </div>
-        <div className="col-lg-8 overflow-hidden">
-          <code style={{ fontSize: `${size ?? 16}px`, whiteSpace: "nowrap" }}>
-            {face}
-          </code>
-        </div>
+        <small className="text-muted">Added {formattedDate}</small>
+        <br />
+        <small> Size: {size} </small>
+      </div>
+      <div className="product-preview p-4 user-select-none d-flex align-items-center">
+        <code style={{ fontSize: `${size}em`, whiteSpace: "nowrap" }}>
+          {face}
+        </code>
       </div>
     </div>
   );

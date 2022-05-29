5737c6ad75f5944ab3be9384b8758d2f14870c8d
AJ <ajabad.dev@gmail.com>
2022-05-29 12:38:47 +0800

    Added styles for product grid

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
+import "./grid.css";
 
 import App from "./App";
 import axios from "axios";

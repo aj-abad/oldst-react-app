14335289cbebf0d855143d1c2ed8de0bc825b0d5
AJ <ajabad.dev@gmail.com>
2022-05-29 12:05:57 +0800

    Created products component

---
+++ src/components/Products.js
@@ -0,0 +1,8 @@
+import { useState, useLayoutEffect, useRef } from "react";
+import { Fragment } from "react";
+import axios from "axios";
+import Ad from "./Ad";
+const Products = (props) => {
+};
+
+export default Products;

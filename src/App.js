02dd9d5ca580bc8009585e0aadd22e453b9a48ca
AJ <ajabad.dev@gmail.com>
2022-05-29 11:55:53 +0800

    Removed ad, moved to its own component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,8 @@ const App = () => {
     <div className='App'>
       <header>
           <h1>Products Grid</h1>
-
-          <p>But first, a word from our sponsors:</p>
-          <img
-            className='ad'
-            src={`http://localhost:8000/ads/?r=${Math.floor(Math.random()*1000)}`}
-            alt='ad'/>
-      </header>
-
-      products goes here..
+        </header>
+        <hr />
     </div>
   );
 }

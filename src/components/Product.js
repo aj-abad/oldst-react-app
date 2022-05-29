7350e2bc6f3572a464ce5c6d4fac511996846432
AJ <ajabad.dev@gmail.com>
2022-05-29 12:26:04 +0800

    Added formatted date and price properties

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,15 @@
+import getDateDiff from "../plugins/getDateDiff";
+
 const Product = ({ product }) => {
   const { date, face, id, price, size } = product;
+
+  const formattedPrice = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format(price / 100);
+
+  const formattedDate = getDateDiff(new Date(date));
+
   return (
     <div className="product p-4">
     </div>

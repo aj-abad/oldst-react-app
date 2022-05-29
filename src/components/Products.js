6b2de87df2f15ef80ad4a8835f29cb84a2d030c1
AJ <ajabad.dev@gmail.com>
2022-05-29 12:22:48 +0800

    Added comments

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,12 +3,21 @@ import { Fragment } from "react";
 import axios from "axios";
 import Ad from "./Ad";
 const Products = (props) => {
+  //define constants
   const PAGE_SIZE = 20;
   const PRODUCTS_BEFORE_AD = 20;
+
+  //define state
   const { lastAd, setLastAd } = props;
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isEndReached, setIsEndReached] = useState(false);
+  
+  /**
+   * I'm primarily a Vue dev so I don't fully get the gotchas of state hooks
+   * Hence my use of useRefs for callbacks outside the render function
+   * Would like to receive feedback on this
+   */
   const sortBy = useRef("");
   const isLoadingRef = useRef(true);
   const cachedProducts = useRef(null);
@@ -16,9 +25,23 @@ const Products = (props) => {
 
   //ref for element that triggers loading products
   const bottomScrollLoader = useRef(null);
+
+  //Run this when the component elements are mounted
   useLayoutEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
+        /**
+         * Has a caveat where if the items are appended when the bottom of the page is visible,
+         * the observer callback won't be triggered until the observed element leaves and re-enters the viewport
+         * 
+         * I could either use the less performant scroll event listener 
+         * 
+         * Or I could check if the observed element is visible
+         * when the products are prefetched and immediately append them
+         *
+         * But it will start getting messy when I accommodate more edge cases
+         * so I decided to just leave it this way
+         */
         if (entries[0].isIntersecting) loadNextPage();
       },
       { threshold: 1 }
@@ -29,6 +52,12 @@ const Products = (props) => {
       observer.disconnect();
     };
   }, []);
+
+  /**
+   *  Function for getting the first page of products
+   *  Gets twice the number of products as the PAGE_SIZE
+   *  And caches half of them for use in the next page
+   */
   const loadInitialProducts = () => {
     axios
       .get(
@@ -51,6 +80,8 @@ const Products = (props) => {
       })
       .catch((err) => alert(err));
   };
+
+  //gets the next page of products
   const prefetchProducts = () => {
     axios
       .get(
@@ -65,6 +96,8 @@ const Products = (props) => {
       })
       .catch((err) => alert(err));
   };
+
+  //function that appends the cached products and pre-fetches the next page
   const loadNextPage = () => {
     if (isLoadingRef.current || !cachedProducts.current) return;
     setProducts((products) => [...products, cachedProducts.current]);
@@ -76,10 +109,12 @@ const Products = (props) => {
     prefetchProducts();
   };
 
+  //function that sorts the products by the given sortBy
   const getIterator = (i, j) => {
     return PAGE_SIZE * i + 1 + j;
   };
 
+  //function that sets the sorting and resets the products
   const setSort = (sort) => {
     setIsEndReached(false);
     sortBy.current = sort;

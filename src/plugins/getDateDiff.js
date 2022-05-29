a4aebe36c6a73706fa128465fb71c96a2f592302
AJ <ajabad.dev@gmail.com>
2022-05-29 12:05:14 +0800

    Added comments

--- src/plugins/getDateDiff.js
+++ src/plugins/getDateDiff.js
@@ -8,15 +8,19 @@ const getDateDiff = (date) => {
   const WEEK_MILLIS = DAY_MILLIS * 7;
 
   const getDaysDiff = (diff) => Math.floor(diff / DAY_MILLIS);
-
+ 
+  //return 'Today' if less than a day ago
   if (diff < DAY_MILLIS) {
     return "today";
   }
+
+  //return the number of days elapsed if less than a week ago
   if (diff < WEEK_MILLIS) {
     const daysDiff = getDaysDiff(diff);
     return `${daysDiff} day${daysDiff !== 1 ? "s" : ""} ago`;
   }
   
+  //return a formatted date if more than a week ago
   return date.toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",

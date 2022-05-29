26c291265ecb0c5bbd37462e2a6c5c0628b74bb3
AJ <ajabad.dev@gmail.com>
2022-05-29 12:00:55 +0800

    Imported and configured axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
+import App from "./App";
+import axios from "axios";
+axios.defaults.baseURL = "http://localhost:8000";
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />

644626d156c8c6edadd7053abc728038d99ee7a2
AJ <ajabad.dev@gmail.com>
2022-05-29 11:57:17 +0800

    Added destructured props and state properties

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -1,4 +1,7 @@
 const Ad = (props) => {
+  const { lastAd, setLastAd } = props;
+  const [src, setSrc] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
   return (
     <div>
       <p>But first, a word from our sponsors:</p>

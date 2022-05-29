954d52d0a30b437a144c7b66fba3856197617f91
AJ <ajabad.dev@gmail.com>
2022-05-29 11:59:47 +0800

    Added function for setting ad src

--- src/components/Ad.js
+++ src/components/Ad.js
@@ -2,6 +2,17 @@ const Ad = (props) => {
   const { lastAd, setLastAd } = props;
   const [src, setSrc] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    let src = Math.floor(Math.random() * 1000);
+    //get a new ad in case of a collision
+    while (src === lastAd) {
+      src = Math.floor(Math.random() * 1000);
+    }
+    //only set the src once we're sure that it's not the same as the last ad
+    setSrc(src);
+  }, []);
+  
   return (
     <div>
       <p>But first, a word from our sponsors:</p>

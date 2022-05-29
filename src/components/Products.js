9318933e87cb5dbea522a9894a571f5e54804248
AJ <ajabad.dev@gmail.com>
2022-05-29 12:11:58 +0800

    Added function for getting the first set of products

--- src/components/Products.js
+++ src/components/Products.js
@@ -16,6 +16,28 @@ const Products = (props) => {
 
   //ref for element that triggers loading products
   const bottomScrollLoader = useRef(null);
+  const loadInitialProducts = () => {
+    axios
+      .get(
+        `products?_page=${currentPage.current}&_limit=${PAGE_SIZE * 2}&${
+          sortBy.current && `_sort=${sortBy.current}`
+        }`
+      )
+      .then(({ data }) => {
+        //slice array in half
+        const half = Math.floor(data.length / 2);
+        const firstHalf = data.slice(0, half);
+        const secondHalf = data.slice(half);
+        //set products to first half
+        setProducts([firstHalf]);
+        //set cached products to second half
+        cachedProducts.current = secondHalf;
+
+        setIsLoading(false);
+        isLoadingRef.current = false;
+      })
+      .catch((err) => alert(err));
+  };
   return (
     <main>
       <div id="scroll-observer" ref={bottomScrollLoader} />

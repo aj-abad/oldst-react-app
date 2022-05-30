f568ad5ca297fc517be7c43b1a4c3750d692221e
AJ <ajabad.dev@gmail.com>
2022-05-30 11:09:33 +0800

    Changed markup

--- src/components/LoadingIndicator.js
+++ src/components/LoadingIndicator.js
@@ -1,17 +1,17 @@
 const LoadingIndicator = () => {
   return (
     <div className="p-5 bg-light d-flex justify-content-center align-items-center">
-      <span className="h5 font-weight-regular">
+      <span className="h5 mb-0">
         <span className="loading-anim-l" aria-hidden="true">
-          ✿•.¸
+          ❤•.¸✿¸.•❤•.❀•
         </span>
-        Loading more products
+        <span className="mx-2">Loading more products</span>
         <span className="loading-anim-r" aria-hidden="true">
-          ¸.•✿
+          •❀.•❤•.¸✿¸.•❤
         </span>
       </span>
     </div>
   );
 };
 
-export default LoadingIndicator
+export default LoadingIndicator;
